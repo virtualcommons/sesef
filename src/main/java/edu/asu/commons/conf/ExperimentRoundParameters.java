596e45f7e95f994b23505bdaa4151b1413000649
alllee <allen.lee@asu.edu>
2012-02-08 16:15:21 -0700

    convoluting type hierarchy even more, ExperimentRoundParameters.Base now inherits functionality from Configuration.Base

--- src/main/java/edu/asu/commons/conf/ExperimentRoundParameters.java
+++ src/main/java/edu/asu/commons/conf/ExperimentRoundParameters.java
@@ -7,13 +7,11 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.stringtemplate.v4.ST;
-
 import edu.asu.commons.util.Duration;
 
 /**
  * $Id$
- *
+ * 
  * Per-round experimental parameters for a given Experiment server instance.
  * 
  * FIXME: can we do mutually recursive generics properly here?
@@ -25,51 +23,47 @@ import edu.asu.commons.util.Duration;
 public interface ExperimentRoundParameters<T extends ExperimentConfiguration> extends Serializable {
 
     public String getInstructions();
-    
+
     public T getParentConfiguration();
+
     public void setParentConfiguration(T configuration);
-    
+
     public Duration getRoundDuration();
-    
+
     public int getRoundNumber();
-    
-//    public Properties getProperties();
-    
-    public static abstract class Base<E extends ExperimentConfiguration> implements ExperimentRoundParameters<E> {
+
+    // public Properties getProperties();
+
+    public static abstract class Base<E extends ExperimentConfiguration> extends Configuration.Base implements ExperimentRoundParameters<E> {
         private static final long serialVersionUID = -7904104481473406817L;
-        private final PropertiesConfiguration assistant = new PropertiesConfiguration();
 
         private final String resource;
         private E parentConfiguration;
-        
+
         public Base() {
             this.resource = null;
         }
-        
+
         public Base(String resource) {
             if (resource == null || "".equals(resource)) {
                 throw new IllegalArgumentException("null or empty resource name: " + resource);
             }
             this.resource = resource;
-            assistant.loadProperties(resource);
+            loadProperties(resource);
         }
-        
+
         public int getRoundNumber() {
             return getParentConfiguration().getAllParameters().indexOf(this);
         }
-        
-        protected PropertiesConfiguration getConfigurationAssistant() {
-            return assistant;
-        }
-        
+
         public void report() {
             report(System.err);
         }
-        
+
         public void report(PrintStream stream) {
-            assistant.getProperties().list(stream);
+            getProperties().list(stream);
         }
-        
+
         public String getResourceFilename() {
             return resource;
         }
@@ -77,22 +71,22 @@ public interface ExperimentRoundParameters<T extends ExperimentConfiguration> ex
         public E getParentConfiguration() {
             return parentConfiguration;
         }
-        
+
         public int getDuration() {
-            return assistant.getIntProperty("duration", getDefaultRoundDuration()); 
+            return getIntProperty("duration", getDefaultRoundDuration());
         }
-        
+
         public Duration getRoundDuration() {
             return Duration.create(getDuration());
         }
-        
 
         public Map<String, Object> toMap() {
-        	return assistant.toMap(this);
+            return toMap(this);
         }
-        
+
         /**
          * Override to set up a different default round duration.
+         * 
          * @return
          */
         protected int getDefaultRoundDuration() {
@@ -103,74 +97,50 @@ public interface ExperimentRoundParameters<T extends ExperimentConfiguration> ex
             this.parentConfiguration = parentConfiguration;
         }
 
-        public boolean getBooleanProperty(String key, boolean defaultValue) {
-            return assistant.getBooleanProperty(key, defaultValue);
-        }
-
         public boolean getBooleanProperty(String key) {
-            return assistant.getBooleanProperty(key, parentConfiguration.getBooleanProperty(key));
-        }
-
-        public double getDoubleProperty(String key, double defaultValue) {
-            return assistant.getDoubleProperty(key, defaultValue);
+            return getBooleanProperty(key, parentConfiguration.getBooleanProperty(key));
         }
 
         public double getDoubleProperty(String key) {
-            return assistant.getDoubleProperty(key, parentConfiguration.getDoubleProperty(key));
-        }
-
-        public int getIntProperty(String key, int defaultValue) {
-            return assistant.getIntProperty(key, defaultValue);
+            return getDoubleProperty(key, parentConfiguration.getDoubleProperty(key));
         }
 
         public int getIntProperty(String key) {
-            return assistant.getIntProperty(key, parentConfiguration.getIntProperty(key));
+            return getIntProperty(key, parentConfiguration.getIntProperty(key));
         }
 
         public Properties getProperties() {
-            return assistant.getProperties();
-        }
-
-        public String getStringProperty(String key, String defaultValue) {
-            return assistant.getStringProperty(key, defaultValue);
+            return getProperties();
         }
 
         public String getStringProperty(String key) {
-            return assistant.getStringProperty(key, parentConfiguration.getProperty(key));
+            return getStringProperty(key, parentConfiguration.getProperty(key));
         }
 
         public void setProperty(String key, String value) {
-            assistant.setProperty(key, value);
+            setProperty(key, value);
         }
 
-        public String getProperty(String key, String defaultValue) {
-            return assistant.getProperty(key, defaultValue);
-        }
-        
         public String getProperty(String key) {
             return getProperty(key, parentConfiguration.getProperty(key));
         }
+
         public boolean isFirstRound() {
             return parentConfiguration.isFirstRound();
         }
+
         public boolean isLastRound() {
             return parentConfiguration.isLastRound();
         }
-        
-        public ST createStringTemplate(String template) {
-            ST st = assistant.createStringTemplate(template);
-            st.add("self", this);
-            return st;
-        }
-        
+
         public long inMinutes(long seconds) {
             return TimeUnit.MINUTES.convert(seconds, TimeUnit.SECONDS);
         }
-        
+
         public long inMinutes(Duration duration) {
             return inMinutes(duration.getTimeLeftInSeconds());
         }
-        
+
         public String toCurrencyString(double amount) {
             return NumberFormat.getCurrencyInstance().format(amount);
         }
