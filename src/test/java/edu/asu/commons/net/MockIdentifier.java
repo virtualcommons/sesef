c59f82bbf3aee3bf819f575681a7be3afe707e9f
alllee <none@none>
2012-02-27 18:33:15 -0700

    potentially useful test scaffolding for concrete experiments

---
+++ src/test/java/edu/asu/commons/net/MockIdentifier.java
@@ -0,0 +1,11 @@
+package edu.asu.commons.net;
+
+/**
+ * Mock identifier class for testing.
+ *
+ */
+public class MockIdentifier extends Identifier.Base<MockIdentifier> {
+
+	private static final long serialVersionUID = -2537405457822734498L;
+	
+}
