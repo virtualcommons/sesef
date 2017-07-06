63731f4599da939f07599edd3be68d2b1b5409ab
Allen Lee <allen.lee@asu.edu>
2017-07-06 09:04:02 -0700

    add summarize boolean to ShowInstructionsRequest
    when true, the client should display summarized instructions

--- src/main/java/edu/asu/commons/event/ShowInstructionsRequest.java
+++ src/main/java/edu/asu/commons/event/ShowInstructionsRequest.java
@@ -3,19 +3,32 @@ package edu.asu.commons.event;
 import edu.asu.commons.net.Identifier;
 
 /**
- * $Id$
- * 
- * 
- * 
+ * Requests that a Client should display its instructions pane.
+ *
  * @author <a href='mailto:allen.lee@asu.edu'>Allen Lee</a>
- * @version $Rev$
  */
 public class ShowInstructionsRequest extends AbstractEvent implements ShowRequest<ShowInstructionsRequest> {
 
     private static final long serialVersionUID = 3774308614796618926L;
 
+    private final boolean summarized;
+
     public ShowInstructionsRequest(Identifier id) {
+        this(id, false);
+    }
+
+    public ShowInstructionsRequest(Identifier id, boolean summarized) {
         super(id);
+        this.summarized = summarized;
+    }
+
+    /**
+     * Returns true if the instructions should be summarized.
+     *
+     * @return true if the instructions should be summarized, false otherwise.
+     */
+    public boolean isSummarized() {
+        return summarized;
     }
 
     @Override
